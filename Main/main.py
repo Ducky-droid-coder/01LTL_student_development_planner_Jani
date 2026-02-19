40fb2a0e75bf6f773f765342bb7bc70c2e8a7fc3
Jani <jaanscoetzer@gmail.com>
2026-02-19 16:43:53 +0200

    Update main.py

--- Main/main.py
+++ Main/main.py
@@ -1,1 +1,6 @@
+import streamlit as st
 
+st.set_page_config(page_title="Streamlit JSON App", layout="centered")
+
+st.title("🚀 Streamlit JSON App")
+st.write("If you can see this, Streamlit is working in Codespaces!")
